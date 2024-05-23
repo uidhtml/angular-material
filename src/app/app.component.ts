3405eca8fc5d2833206c02b29856dde3e8ca9ca0
uidhtml <54753484+uidhtml@users.noreply.github.com>
2024-05-23 11:29:15 +0530

    Added button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MatButtonModule,
+} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { A11yModule, FocusMonitor, FocusOrigin } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, A11yModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [
+    {
+      provide: MAT_BUTTON_CONFIG,
+      useValue: {
+        disabledInteractive: true,
+      },
+    },
+    {
+      provide: MAT_FAB_DEFAULT_OPTIONS,
+      useValue: {
+        color: 'primary',
+      },
+    },
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'material-design';
+  buttonColor: string = 'primary';
+
+  constructor(private focusMonitor: FocusMonitor, private elmRef: ElementRef) {}
+
+  ngOnInit() {
+    this.focusMonitor
+      .monitor(this.elmRef.nativeElement, true)
+      .subscribe((origin: FocusOrigin) => {
+        console.log(origin);
+      });
+  }
 }
